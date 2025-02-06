b389bd2e7334d6521330adc8c419fe3b0fa07a5c
andref257 <af211733@gmail.com>
2025-02-06 19:26:10 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const lista = document.getElementById("lista de amigos");
+const resultado = document.getElementById("resultado");
+let = []
+
+function adicionarAmigo() {
+  let inputName = document.getElementsByClassName('amigo').value
+  if (inputName == "") {
+    alert('campo de nome está vazio');
+  } else {
+    Array.push(inputName)
+    document.getElementById()
+  }
+}
